f599da6cae4ef23f626f38c673bb1497e633cb2d
Reee00 <ahmadd.renaldyy@gmail.com>
2026-03-07 01:06:45 +0700

    fix fast excel

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,6 +6,7 @@ use App\Models\SchoolClass;
 use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Rap2hpoutre\FastExcel\FastExcel;
 
 class StudentController extends Controller
 {
